bba2af3ea9fb53a3565cbbb9e006f20ec979ee4a
GodSlappedMe <narampark92@gmail.com>
2023-10-02 13:02:28 -0400

    future forecast js added

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@ function getCoordinates(city) {
         const longitude = data.coord.lon;
         console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
         getCurrentWeather(latitude, longitude);
+        getFutureForecast(latitude, longitude);
       } else {
         console.error("Unable to retrieve coordinates for the city.");
       }
@@ -21,21 +22,24 @@ function getCoordinates(city) {
 }
 
 function getCurrentWeather(latitude, longitude) {
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
   fetch(weatherUrl)
     .then((response) => response.json())
     .then((data) => {
-      const cityName = data.city.name;
-      const temperature = data.list[0].main.temp;
-      const humidity = data.list[0].main.humidity;
-      const windSpeed = data.list[0].wind.speed;
-      const icon = data.list[0].weather[0].icon;
+      const cityName = data.name;
+      const temperatureCelsius = data.main.temp - 273.15;
+      const temperatureFahrenheit = (temperatureCelsius * 9) / 5 + 32;
+      const humidity = data.main.humidity;
+      const windSpeed = data.wind.speed;
+      const icon = data.weather[0].icon;
 
       document.getElementById("city-name").textContent = cityName;
       document.getElementById(
         "temperature"
-      ).textContent = `Temperature: ${temperature}°C`;
+      ).textContent = `Temperature: ${temperatureFahrenheit.toFixed(
+        2
+      )}°F (${temperatureCelsius.toFixed(2)}°C)`;
       document.getElementById(
         "humidity"
       ).textContent = `Humidity: ${humidity}%`;
@@ -45,6 +49,37 @@ function getCurrentWeather(latitude, longitude) {
       document.getElementById(
         "weather-icon"
       ).src = `https://openweathermap.org/img/wn/${icon}.png`;
+    });
+}
+
+function getFutureForecast(latitude, longitude) {
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+  fetch(forecastUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < 5; i++) {
+        const forecast = data.list[i * 8];
+        const dateElement = document.querySelector(`.date-${i + 1}`);
+        const temperatureElement = document.querySelector(
+          `.temperature-${i + 1}`
+        );
+        const humidityElement = document.querySelector(`.humidity-${i + 1}`);
+        const windSpeedElement = document.querySelector(`.wind-speed-${i + 1}`);
+        const iconElement = document.querySelector(`.w-icon-${i + 1}`);
+
+        const forecastDate = new Date(forecast.dt * 1000);
+        const temperatureCelsius = forecast.main.temp - 273.15;
+        const temperatureFahrenheit = (temperatureCelsius * 9) / 5 + 32;
+
+        dateElement.textContent = forecastDate.toDateString();
+        temperatureElement.textContent = `Temperature: ${temperatureFahrenheit.toFixed(
+          2
+        )}°F (${temperatureCelsius.toFixed(2)}°C)`;
+        humidityElement.textContent = `Humidity: ${forecast.main.humidity}%`;
+        windSpeedElement.textContent = `Wind Speed: ${forecast.wind.speed} m/s`;
+        iconElement.src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
+      }
     });
 }
 
